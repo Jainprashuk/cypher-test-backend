89a698f0596839d6d541925006b91ed08d719dfe
Anon <ajeet.singh@lpu.in>
2024-08-20 18:06:43 +0530

    ha

--- Utils/generateTokens.js
+++ Utils/generateTokens.js
@@ -4,7 +4,7 @@ const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ _id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV !== 'production',
     sameSite: 'strict',
     maxAge: 3600000,
   });
